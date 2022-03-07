1ee66f4cc8204d3a3cde21282c79669d11b6f475
Dewaley <adewaleademilua@gmail.com>
2022-03-07 09:19:59 +0100

    Fixed tailwind installation

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function App() {
   return (
-    <div className="">
-      <h1>Hello World</h1>
+    <div>
+      <h1 className='text-6xl'>Hello World</h1>
     </div>
   );
 }
